babb3a62e785badc1b2eba95216f52c2f5c6a4ae
githubfoam <lequestionis@gmail.com>
2020-04-28 17:14:54 +0300

    Is that a guy or a girl singin' that song?

---
+++ hackerrank/src/hackerrank/algorithms/search/TripleSum.java
@@ -0,0 +1,82 @@
+// https://www.hackerrank.com/challenges/triple-sum/problem?h_l=interview&playlist_slugs%5B%5D%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D%5B%5D=search
+package hackerrank.algorithms.search;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class TripleSum {
+
+	 // Complete the triplets function below.
+    static long triplets(int[] a, int[] b, int[] c) {
+        int[] aSet = Arrays.stream(a).distinct().sorted().toArray(); // Set implementation
+        int[] bSet = Arrays.stream(b).distinct().sorted().toArray();
+        int[] cSet = Arrays.stream(c).distinct().sorted().toArray();
+        long count = 0;
+        for (int second : bSet) {
+            long ac = Math.abs(Arrays.binarySearch(aSet, second) + 1);
+            long cc = Math.abs(Arrays.binarySearch(cSet, second) + 1);
+            count += ac * cc;
+        }
+        return count;
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+//        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] lenaLenbLenc = scanner.nextLine().split(" ");
+
+        int lena = Integer.parseInt(lenaLenbLenc[0]);
+
+        int lenb = Integer.parseInt(lenaLenbLenc[1]);
+
+        int lenc = Integer.parseInt(lenaLenbLenc[2]);
+
+        int[] arra = new int[lena];
+
+        String[] arraItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < lena; i++) {
+            int arraItem = Integer.parseInt(arraItems[i]);
+            arra[i] = arraItem;
+        }
+
+        int[] arrb = new int[lenb];
+
+        String[] arrbItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < lenb; i++) {
+            int arrbItem = Integer.parseInt(arrbItems[i]);
+            arrb[i] = arrbItem;
+        }
+
+        int[] arrc = new int[lenc];
+
+        String[] arrcItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < lenc; i++) {
+            int arrcItem = Integer.parseInt(arrcItems[i]);
+            arrc[i] = arrcItem;
+        }
+
+        long ans = triplets(arra, arrb, arrc);
+        System.out.println(ans);
+//
+//        bufferedWriter.write(String.valueOf(ans));
+//        bufferedWriter.newLine();
+//        bufferedWriter.close();
+
+        scanner.close();
+    }
+
+}
---
+++ hackerrank/src/hackerrank/algorithms/search/TripleSum1.java
@@ -0,0 +1,94 @@
+// https://www.hackerrank.com/challenges/triple-sum/problem?h_l=interview&playlist_slugs%5B%5D%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D%5B%5D=search
+package hackerrank.algorithms.search;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class TripleSum1 {
+
+	static long binarysearchCustom(int[] arr, int key) {
+		int index=-1,low=0,mid=0, high=arr.length-1;
+		while (high >= low) {
+			mid= low + (high-low) /2;
+			if ( key >= arr[mid] ) {
+				index=mid;
+				low = mid+1;
+			}else {
+				high=mid-1;
+			}
+		}
+		return index;
+	}
+	
+    static long triplets(int[] a, int[] b, int[] c) {
+        int[] aSet = Arrays.stream(a).distinct().sorted().toArray(); // Set implementation
+        int[] bSet = Arrays.stream(b).distinct().sorted().toArray();
+        int[] cSet = Arrays.stream(c).distinct().sorted().toArray();
+        long count = 0;
+        for (int second : bSet) {
+            long ac = Math.abs(binarysearchCustom(aSet, second) + 1);
+            long cc = Math.abs(binarysearchCustom(cSet, second) + 1);
+            count += ac * cc;
+        }
+        return count;
+    }
+	
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+//        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] lenaLenbLenc = scanner.nextLine().split(" ");
+
+        int lena = Integer.parseInt(lenaLenbLenc[0]);
+
+        int lenb = Integer.parseInt(lenaLenbLenc[1]);
+
+        int lenc = Integer.parseInt(lenaLenbLenc[2]);
+
+        int[] arra = new int[lena];
+
+        String[] arraItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < lena; i++) {
+            int arraItem = Integer.parseInt(arraItems[i]);
+            arra[i] = arraItem;
+        }
+
+        int[] arrb = new int[lenb];
+
+        String[] arrbItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < lenb; i++) {
+            int arrbItem = Integer.parseInt(arrbItems[i]);
+            arrb[i] = arrbItem;
+        }
+
+        int[] arrc = new int[lenc];
+
+        String[] arrcItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < lenc; i++) {
+            int arrcItem = Integer.parseInt(arrcItems[i]);
+            arrc[i] = arrcItem;
+        }
+
+        long ans = triplets(arra, arrb, arrc);
+        System.out.println(ans);
+//
+//        bufferedWriter.write(String.valueOf(ans));
+//        bufferedWriter.newLine();
+//        bufferedWriter.close();
+
+        scanner.close();
+    }
+
+}
